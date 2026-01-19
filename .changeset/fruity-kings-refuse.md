b19041c5addbf40c289afb34edc890681aaa6332
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-01-20 00:52:51 +0100

    fix: Default check for react-devtools-core package

---
+++ .changeset/fruity-kings-refuse.md
@@ -0,0 +1,5 @@
+---
+"@kubb/react-fabric": patch
+---
+
+Default check for react-devtools-core package
--- packages/react-fabric/src/devtools.ts
+++ packages/react-fabric/src/devtools.ts
@@ -11,6 +11,8 @@ declare global {
   var isDevtoolsEnabled: any
 }
 
+let isOpen = false
+
 export function openDevtools() {
   // Set up global polyfills BEFORE importing react-devtools-core
   // This is required because react-devtools-core expects these to be available
@@ -86,15 +88,19 @@ export function openDevtools() {
   import('react-devtools-core').then(async (devtools) => {
     console.info('Opening devtools')
     const controller = new AbortController()
-    execa({
-      stdio: 'pipe',
-      preferLocal: true,
-      cancelSignal: controller.signal,
-      gracefulCancel: true,
-    })`npx react-devtools@6.1.5`
+    if (!isOpen) {
+      execa({
+        stdio: 'pipe',
+        preferLocal: true,
+        cancelSignal: controller.signal,
+        gracefulCancel: true,
+      })`npx react-devtools@6.1.5`
+    }
+
+    isOpen = true
 
     // Destructure the functions from the module
-    const { initialize, connectToDevTools } = devtools
+    const { initialize, connectToDevTools } = devtools?.default || devtools
 
     // Initialize DevTools BEFORE importing Renderer (which imports React)
     initialize()
